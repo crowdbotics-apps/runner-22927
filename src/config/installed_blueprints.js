0df981d257744c10f7fab868ab69d71c31d4b8ec
Crowdbotics <team@crowdbotics.com>
2020-11-23 16:09:33 +0000

    add package blueprint-react-native-message-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Messaging3178100', human_name: 'Messaging 3', access_route: 'Messaging3178100', icon: 'comments'},
 { name: 'Messaging4178099', human_name: 'Messaging 4', access_route: 'Messaging4178099', icon: 'comments'},
 { name: 'Messaging5178098', human_name: 'Messaging 5', access_route: 'Messaging5178098', icon: 'comments'},
 { name: 'Messaging6178097', human_name: 'Messaging 6', access_route: 'Messaging6178097', icon: 'comments'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Messaging3178100Navigator from '../features/Messaging3178100/navigator';
 import Messaging4178099Navigator from '../features/Messaging4178099/navigator';
 import Messaging5178098Navigator from '../features/Messaging5178098/navigator';
 import Messaging6178097Navigator from '../features/Messaging6178097/navigator';
@@ -20,6 +21,7 @@ import BlankScreen0178093Navigator from '../features/BlankScreen0178093/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Messaging3178100: { screen: Messaging3178100Navigator },
 Messaging4178099: { screen: Messaging4178099Navigator },
 Messaging5178098: { screen: Messaging5178098Navigator },
 Messaging6178097: { screen: Messaging6178097Navigator },
